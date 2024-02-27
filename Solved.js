421df4ed934a91585022881ec970990a6ab2b258
Brie Morris <129684030+BrieMorris@users.noreply.github.com>
2024-02-27 16:04:47 -0600

    basic mathematical operations

--- Solved.js
+++ Solved.js
@@ -29,6 +29,29 @@ function squareDigits(num){
 
 //8 ky
 
+//Basic Math operations - solution
+function basicOp(operation, value1, value2){
+  switch(operation){
+      case '+':
+        return value1 + value2;
+      case '-':
+        return value1 - value2;
+      case '*':
+        return value1 * value2;
+      case '/':
+        return value1 / value2;
+      default:
+        return "Invalid operation";
+  }
+  return 0;
+}
+
+//another option
+function basicOp(operation, value1, value2)
+{
+  return eval(value1 + operation + value2);
+}
+
 //sum of positive
 function positiveSum(arr) {
   let positiveNumArr = []
